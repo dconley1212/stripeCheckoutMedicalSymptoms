c90bece3e32cc4dd712d14df41d70aec347596c9
David Conley <dconley1989@gmail.com>
2022-11-04 17:43:02 -0600

    added the stripe payment intent for stripes payment element type

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
-const Your_Domain = "http://localhost:8080";
+const port = process.env.PORT 
+
+const calculateOrderAmount = (items) =>{
+    return 4400
+}
+
+
+app.post('/create-payment-intent', async (req, res) =>{
+    const {items} = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true;
+        }
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+
+
+app.listen(port, () =>{
+console.log("Server is listening")
+})
