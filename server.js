5b218fd84bc7cff56760b397a0e5853b5d65c718
David Conley <dconley1989@gmail.com>
2022-11-08 14:34:55 -0700

    updated the calculate order amount function and added cors object to the route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const path = require("path");
-const cors = require('cors')
+const cors = require("cors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
@@ -8,34 +8,34 @@ const app = express();
 // app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT 
+const port = process.env.PORT;
 
 const corsOptions = {
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200,
-}
+  origin: "http://localhost:3000/checkout",
+  optionsSuccessStatus: 200,
+};
 
-const calculateOrderAmount = (items) =>{
-    return 4400
-}
+const calculateOrderAmount = (items) => {
+  let totalCost = 0;
+  items.forEach((item) => (totalCost += item.price));
+  return totalCost;
+};
 
+app.post("/create-payment-intent", corsOptions, async (req, res) => {
+  const { items } = req.body;
 
-app.post('/create-payment-intent', async (req, res) =>{
-    const {items} = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(items),
-        currency: "usd",
-        automatic_payment_methods: {
-            enabled: true;
-        }
-    });
-    res.send({
-        clientSecret: paymentIntent.client_secret,
-    })
-})
-
-
-app.listen(port, () =>{
-console.log("Server is listening")
-})
+app.listen(port, () => {
+  console.log("Server is listening");
+});
